efb3134926e36f5e907b854108793ad7ad8818cb
phantomlord319 <ankit.dubey319@gmail.com>
2020-01-13 21:04:59 +0530

    Updates

---
+++ precache-manifest.ae72a2d97f53a9d20241555a762a817b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26d02d06221753c4c79ed6cd32db1dd",
+    "url": "/clientpanel/index.html"
+  },
+  {
+    "revision": "6cc70b6a038c2464b73d",
+    "url": "/clientpanel/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3f2683d5415dc2e0265b",
+    "url": "/clientpanel/static/js/2.fc37d79b.chunk.js"
+  },
+  {
+    "revision": "f9e698cf68ebaeaaae24d97eb2daf42f",
+    "url": "/clientpanel/static/js/2.fc37d79b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cc70b6a038c2464b73d",
+    "url": "/clientpanel/static/js/main.49300dbb.chunk.js"
+  },
+  {
+    "revision": "9bfc0c9749b27b375fac",
+    "url": "/clientpanel/static/js/runtime-main.902e8fee.js"
+  }
+]);
